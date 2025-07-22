bbcd44d2e34cf7b23295198a5f6a952c3f14c7a6
Fuad Hüseynov <fuadhuseynov1202@gmail.com>
2025-07-22 06:39:03 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,289 @@
+
+const data = {
+  "Şaqalatlar": [
+    { ad: "NEVSKİY PETROVSKİ FORD", kod: "271 6908", sekil: "2716908.jpg" },
+    { ad: "SANAY RACKİ KARAMEL", kod: "271 7071", sekil: "2717071.jpg" },
+    { ad: "ROSHEN JELLY", kod: "271 6894", sekil: "2716894.jpg" },
+    { ad: "NEVSKİY BATONCİK SLİNOCNİY", kod: "271 8429", sekil: "2718429.jpg" },
+    { ad: "KONTİ SLADKOE SOZVUCİE MORODİNA", kod: "271 6652", sekil: "2716652.jpg" },
+    { ad: "KONTİ SLADKOE SOZVUCİE APELSİN", kod: "271 6651", sekil: "2716651.jpg" },
+    { ad: "MONTE LEMO MİLK CHOCOLATE", kod: "271 7158", sekil: "2717158.jpg" },
+    { ad: "SLADUNİCA SNEQ V TEMNOM SHOK SUFLE", kod: "271 8533", sekil: "2718533.jpg" },
+    { ad: "R.F SKAZKA KARAMEL", kod: "271 8154", sekil: "2718154.jpg" },
+    { ad: "AİDİN 7 CİN TOFFE MİX", kod: "271 8138", sekil: "2718138.JPG" },
+        { ad: "ŞİRİN SWALLOV ŞOKALAD", kod: "271 8009", sekil: "2718009.jpg" },
+    { ad: "SHONİZ MEYVƏLİ KARAMEL", kod: "271 6392", sekil: "2716392.jpg" },
+    { ad: "KONFİRON WALKERS", kod: "271 8088", sekil: "2718088.jpg" },
+    { ad: "RAHAT ARAXİSLİ KARAMEL", kod: "271 8337", sekil: "2718337.jpg" },
+    { ad: "SHELALE MEYVƏLİ TURŞ KARAMEL", kod: "271 8579", sekil: "2718579.jpg" },
+    { ad: "ELVAN TOFFİX MİX", kod: "271 7066", sekil: "2717066.jpg" },
+    { ad: "KDV QLEYS QAYMAQLI KAKOSLU", kod: "271 8189", sekil: "2718189.jpg" },
+    { ad: "KDV JELEYNİYE VİSNE", kod: "271 7305", sekil: "2717305.jpg" },
+    { ad: "ORELAY MAREDO KARAMEL BARBARİS ", kod: "271 8593", sekil: "2718593.jpg" },
+    { ad: "KDV BERRY ART QARAGİLE", kod: "271 8191", sekil: "2718191.jpg" },
+        { ad: "MAREDO İRİS", kod: "271 8516", sekil: "2718516.jpg" },
+    { ad: "NORMO NİLİTO KLASSİK", kod: "271 8339", sekil: "2718339.jpg" },
+    { ad: "KDV VERSAL", kod: "271 8190", sekil: "2718190.jpg" },
+    { ad: "ORELAY MAREDO KARAMEL NANƏ", kod: "271 8592", sekil: "2718592.jpg" },
+    { ad: "ROSHEN DUŞES KARAMEL", kod: "271 8550", sekil: "2718550.jpg" },
+    { ad: "MAREDO DREAM WAY", kod: "271 7309", sekil: "2717309.jpg" },
+    { ad: "MAREDO DREAM NİGHT", kod: "271 7307", sekil: "2717307.jpg" },
+    { ad: "AİDİN MEYVƏLİ KARAMEL", kod: "271 7156", sekil: "2717156.jpg" },
+    { ad: "MAREDO DREAM  LAND", kod: "271 7308", sekil: "2717308.jpg" },
+    { ad: "ORELAY MAREDO KARAMEL GAVALI", kod: "271 8306", sekil: "27183091.jpg" },
+        { ad: "ROSHEN RACKİ", kod: "271 6893", sekil: "2716893.jpg" },
+    { ad: "SOFFİ LOKUM MEYVƏLİ", kod: "271 6126", sekil: "2716126.jpg" },
+    { ad: "AİDİN ŞOKALADLI RACKİ", kod: "271 7137", sekil: "2717137.jpg" },
+    { ad: "KONTİ SİVKUSOM", kod: "271 6852", sekil: "2716852.jpg" },
+    { ad: "KAZİWEH MEYVƏLİ BONBONS KARAMEL", kod: "271 8334", sekil: "2718334.jpg" },
+    { ad: "SHONİZ DİABET KARAMEL", kod: "271 6420", sekil: "2716420.jpg" },
+    { ad: "MAREDO SMEX YERFISTIQLI", kod: "271 6332", sekil: "2716332.jpg" },
+    { ad: "ROSHEN LOLİPOP KARAMEL", kod: "271 7997", sekil: "2717997.jpg" },
+    { ad: "MAREDO KAKOSOVİY MİR", kod: "271 6902", sekil: "2716902.jpg" },
+    { ad: "R.F KARAMEL LİMONAYA", kod: "271 6777", sekil: "2716777.jpg" },
+        { ad: "QAZAX NANƏ KARAMEL", kod: "271 7218", sekil: "2717218.jpg" },
+    { ad: "AİDİN RACKİ", kod: "271 7993", sekil: "2717993.jpg" },
+    { ad: "HASAR SÜDLÜ İRİS", kod: "271 8321", sekil: "2718321.jpg" },
+    { ad: "MAREDO FREE KARAMEL BARBARİS 5X1 ", kod: "271 5864", sekil: "2715864.jpg" },
+    { ad: "ROSHEN BESHENAYA PCLENKA ", kod: "271 7139", sekil: "2717139.jpg" },
+    { ad: "ROSHEN MİLKY SPLASH KQ STAKAN MALAKO", kod: "271 6259", sekil: "2716259.jpg" },
+    { ad: "MAREDO COCO SEA ", kod: "271 5930", sekil: "2715930.jpg" },
+    { ad: "R.F MECTA KARAMEL ", kod: "271 6355", sekil: "2716355.jpg" },
+    { ad: "DEYA CAPİTAN MİLLER VAFLİ", kod: "271 8626", sekil: "2718626.jpg" },
+    { ad: "NEVSKİY SLİVOCHNAYA MU CREAMY NOUGOT", kod: "271 6789", sekil: "2716789.jpg" },
+        { ad: "ORELAY MAREDO KARAMEL LİMON", kod: "271 8307", sekil: "2718307.jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+        { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+        { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+        { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+        { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+        { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+        { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+        { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" },
+    { ad: "məhsul", kod: "0000", sekil: ".jpg" }
+  ],
+  "Toyuq məhsulları": [
+    { ad: "Toyuq filesi", kod: "201", sekil: "toyuq.jpg" },
+    { ad: "Toyuq budu", kod: "202", sekil: "bud.jpg" }
+  ],
+  "Kolbasa": [
+    { ad: "Suxoy kolbasa", kod: "301", sekil: "suxoy.jpg" },
+    { ad: "Doktorskaya kolbasa", kod: "302", sekil: "doktor.jpg" }
+  ],
+  "Sosiska": [
+    { ad: "Qaynadılmış sosiska", kod: "401", sekil: "sosis.jpg" }
+  ],
+  "Pendir": [
+    { ad: "Ağ pendir", kod: "501", sekil: "pendir.jpg" }
+  ],
+  "Meyvə-tərəvəz": [
+    { ad: "BANAN", kod: "271 6629", sekil: "2716629.PNG" },
+    { ad: "KƏLƏM", kod: "271 6688", sekil: "2716688.PNG" },
+    { ad: "YAŞIL ALMA", kod: "271 7988 ", sekil: "2717988.PNG" },
+    { ad: "TƏRXUN", kod: "261 4010", sekil: "2614010.jpg" },
+    { ad: "AVOKADO adı", kod: "261 4557", sekil: "2614557.jpg" },
+    { ad: "KAHI", kod: "261 4557", sekil: "2614557.jpg" },
+    { ad: "QARĞIDALI", kod: "261 5053", sekil: "2615053.PNG" },
+    { ad: "ANANAS", kod: "261 5339", sekil: "2615339.jpg" },
+    { ad: "ZƏNCƏFİL", kod: "271 6345", sekil: "2716345.jpg" },
+    { ad: "ŞAFTALI NEKTARI", kod: "271 6370", sekil: "2716370.PNG" },
+   { ad: "ARMUD", kod: "271 6473", sekil: "2716473.JPEG" },
+   { ad: "YER FISTIĞII QABIQLI", kod: "271 6475", sekil: "2716475.JPG" },
+   { ad: "ACI BİBƏR", kod: "2716484", sekil: "2716484.PNG" },
+      { ad: "QIRMIZI ALMA", kod: "271 6536", sekil: "2716536.PNG" },
+   { ad: "PORTAĞAL", kod: "271 6630", sekil: "2716630.JPG" },
+   { ad: "KARTOF", kod: "271 6631", sekil: "2716631.PNG" },
+      { ad: "XİYAR MELİT", kod: "271 6639", sekil: "2716639.png" },
+   { ad: "SOGAN RUS", kod: "271 6640", sekil: "2716640.PNG" },
+   { ad: "SOĞAN", kod: "271 6685", sekil: "2716685.PNG" },
+      { ad: "POMİDOR", kod: "271 6686", sekil: "2716686.PNG" },
+   { ad: "XİYAR YELPƏNƏK", kod: "271 6770", sekil: "2716770.jpg" },
+   { ad: "ŞİRİN BİBƏR", kod: "271 6800", sekil: "2716800.jpg" },
+      { ad: "LİMON", kod: "271 6858", sekil: "2716858.PNG" },
+   { ad: "RƏNGLİ BİBƏR", kod: "271 6931", sekil: "2716931.jpg" },
+   { ad: "GÜL KƏLƏM", kod: "271 6943", sekil: "2716943.jpg" },
+{ ad: "ŞAFTALI", kod: "271 7205", sekil: "2717205.PNG" },
+ { ad: "GÖYƏRTİ", kod: "271 7225", sekil: "2717225.jpg" },
+   { ad: "BADIMCAN", kod: "271 7233", sekil: "2717233.PNG" },
+{ ad: "LOBYA", kod: "271 7234", sekil: "2717234.JPEG" },
+ { ad: "SARIMSAQ", kod: "271 7235", sekil: "2717235.PNG" },
+   { ad: "BADAM", kod: "271 7274", sekil: "2717274.PNG" },
+{ ad: "YEMİŞ", kod: "271 7384", sekil: "2717384.PNG" },
+ { ad: "QARPIZ", kod: "271 7468", sekil: "2717468.PNG" },
+   { ad: "BALQABAQ", kod: "271 7654", sekil: "2717654.jpg" },
+{ ad: "ÇUĞUNDUR", kod: "271 7665", sekil: "2717665.PNG" },
+ { ad: "KİVİ", kod: "271 7803", sekil: "2717803.jpg" },
+   { ad: "ŞAFTALI YASTI", kod: "271 8127", sekil: "2718127.jpg" },
+   { ad: "KÖK", kod: "2718151", sekil: "2718151.PNG" },
+{ ad: "POMİDOR MUCO", kod: "271 8590", sekil: "2718590.jpg" },
+{ ad: "QIRMIZI KƏLƏM", kod: "271 8591 ", sekil: "2718591.jpg" },
+    { ad: "GÖBƏLƏK", kod: "2000040692171", sekil: "2000040692171.jpg" }
+  ]
+};
+
+const categoriesDiv = document.getElementById("categories");
+const productsSection = document.getElementById("products-section");
+const categoryTitle = document.getElementById("category-title");
+const productList = document.getElementById("product-list");
+
+Object.keys(data).forEach((category) => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerText = category;
+  card.onclick = () => showProducts(category);
+  categoriesDiv.appendChild(card);
+});
+
+let currentCategory = "";
+let filteredProducts = [];
+
+function showProducts(category) {
+  currentCategory = category;
+  categoriesDiv.style.display = "none";
+  productsSection.classList.remove("hidden");
+  categoryTitle.innerText = category;
+  document.getElementById("search").value = "";
+  filteredProducts = data[category];
+  renderProducts();
+}
+
+function renderProducts() {
+  productList.innerHTML = "";
+  filteredProducts.forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    if (item.sekil) {
+      const img = document.createElement("img");
+      img.src = "images/" + item.sekil;
+      card.appendChild(img);
+    }
+    const name = document.createElement("p");
+    name.innerText = item.ad;
+    const code = document.createElement("p");
+    code.innerHTML = `<strong>Kod:</strong> ${item.kod}`;
+    card.appendChild(name);
+    card.appendChild(code);
+    productList.appendChild(card);
+  });
+}
+
+function searchProducts() {
+  const query = document.getElementById("search").value.toLowerCase();
+  filteredProducts = data[currentCategory].filter(item =>
+    item.ad.toLowerCase().includes(query)
+  );
+  renderProducts();
+}
+
+function goBack() {
+  productsSection.classList.add("hidden");
+  categoriesDiv.style.display = "grid";
+}
+
+
+function renderProducts() {
+  productList.innerHTML = "";
+  if (filteredProducts.length === 0) {
+    productList.innerHTML = "<p style='text-align:center;'>Təəssüf ki, heç bir məhsul tapılmadı.</p>";
+    return;
+  }
+
+  filteredProducts.forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    if (item.sekil) {
+      const img = document.createElement("img");
+      img.src = "images/" + item.sekil;
+      card.appendChild(img);
+    }
+
+    const name = document.createElement("p");
+    name.innerText = item.ad;
+
+    const code = document.createElement("p");
+    code.innerHTML = `<strong>Kod:</strong> ${item.kod}`;
+    code.style.cursor = "pointer";
+    code.style.color = "#000000ff";
+    code.title = "Kliklə kopyala";
+
+    code.onclick = () => {
+      navigator.clipboard.writeText(item.kod);
+      alert("Kod panoya kopyalandı: " + item.kod);
+    };
+
+    card.appendChild(name);
+    card.appendChild(code);
+    productList.appendChild(card);
+  });
+}
